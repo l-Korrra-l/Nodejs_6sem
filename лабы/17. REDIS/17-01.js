46167ef830dc4e3fd41ad2eb7abd86e379429bd9
l-Korrra-l <64279692+l-Korrra-l@users.noreply.github.com>
2022-06-16 22:59:34 +0300

    make it better

--- лабы/17. REDIS/17-01.js
+++ лабы/17. REDIS/17-01.js
@@ -1,7 +1,7 @@
 const redis=require('redis');
 
 (async () => {
-  const client = redis.createClient({ url: 'redis://redis-12730.c275.us-east-1-4.ec2.cloud.redislabs.com:12730'})
+  const client = redis.createClient({ url: 'redis://kora:kora123A1!@redis-12730.c275.us-east-1-4.ec2.cloud.redislabs.com:12730'})
   client.on('error', (err) => console.log('Redis Client Error', err));
   client.on('ready',()=>{console.log('ready')});
   client.on('connect',()=>{console.log('connect')});
